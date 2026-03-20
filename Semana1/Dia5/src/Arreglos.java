706b933fc3082cde97385e12603f05d970361b69
AldoYedra99 <aldojonat@gmail.com>
2026-03-19 21:17:04 -0600

    arreglos

---
+++ Semana1/Dia5/src/Arreglos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Arreglos {
+    public static void main(String []args){
+        Scanner obj = new Scanner(System.in);
+
+        /*int[] numeros = {10,20,30,40,50};
+        for(int i=0; i<numeros.length; i++){
+            System.out.println(numeros[i]);
+        }*/
+
+
+    }
+}
