6c7dd2b0512ee80ec93d507bcd28bb76c4842132
AldoYedra99 <aldojonat@gmail.com>
2026-04-05 15:15:35 -0600

    Update

--- Semana4/S4Dia2/src/Main.java
+++ Semana4/S4Dia2/src/Main.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String [] args){
+    public static void main(String [] args) {
         Scanner obj = new Scanner(System.in);
         ArrayList<Usuario> usuarios = new ArrayList<>();
 
@@ -10,8 +10,8 @@ public class Main {
         int cantidad = obj.nextInt();
         obj.nextLine();
 
-        for (int i=0; i<cantidad; i++){
-            System.out.print("Usuario ("+(i+1)+") --> ");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.print("Usuario (" + (i + 1) + ") --> ");
 
             System.out.print("Nombre: ");
             String nombre = obj.nextLine();
@@ -20,12 +20,21 @@ public class Main {
             int edad = obj.nextInt();
             obj.nextLine();
 
-            usuarios.add(new Usuario(nombre,edad));
+            usuarios.add(new Usuario(nombre, edad));
         }
-        System.out.print("Ingrese edad: ");
+        //Filtrar por edad
+        /*System.out.print("Ingrese edad: ");
         int num = obj.nextInt();
         usuarios.stream()
                 .filter(u -> u.edad >= num)
-                .forEach(u -> System.out.println(u.nombre+" | "+u.edad));
+                .forEach(u -> System.out.println(u.nombre+" | "+u.edad));*/
+
+        //Buscar por nombre
+        System.out.print("Ingrese Nombre del usuario: ");
+        String nom = obj.nextLine();
+        usuarios.stream()
+                .filter(u -> u.nombre.equals(nom))
+                .findFirst()
+                .ifPresent(u -> System.out.println(u.nombre+" | "+u.edad));
     }
 }
