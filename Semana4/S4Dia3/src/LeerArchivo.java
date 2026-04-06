8b5de5ffcb72266e6403fa40420f91e583efe794
AldoYedra99 <aldojonat@gmail.com>
2026-04-06 13:15:44 -0600

    Archivos

--- Semana4/S4Dia3/src/LeerArchivo.java
+++ Semana4/S4Dia3/src/LeerArchivo.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class LeerArchivo {
+    public static void main(String[] args) {
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("usuarios.txt"));
+            String linea;
+             while((linea = reader.readLine()) != null){
+                 System.out.println(linea);
+             }
+
+             reader.close();
+        }catch (IOException e){
+            System.out.println("Error al leer el archivo");
+        }
+    }
 }
